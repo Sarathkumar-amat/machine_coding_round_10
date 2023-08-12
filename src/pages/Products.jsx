82e553c828aaaa1ffe9391aaf1c40a4c6980900d
Sarathkumar-amat <poongodinall@gmail.com>
2023-08-12 21:48:22 +0530

    added filters

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { InventoryContext } from "../context/InventoryProvider"
 import { Link, useNavigate } from "react-router-dom";
 
@@ -6,10 +6,43 @@ export function Products()
 {
     const {prodList,setProdList,productData,dispatchProduct} = useContext(InventoryContext);
     const navigate = useNavigate();
+    const [lowStock,setLowStock] = useState(false);
+    const [sortType,setSortType] = useState("");
     let displayData = prodList;
-
+    if(productData?.deptName!=="")
+    {
+        displayData=displayData?.filter(({department})=>department===productData?.deptName)
+    }
+    if(lowStock)
+    {
+        displayData = displayData?.filter(({stock})=>stock<=10);
+    }
+    const handleLowStock = (e)=>{
+        if(e.target.checked)
+        {
+            setLowStock(true);
+        }
+        else{
+            setLowStock(false);
+        }
+    }
+    
+  
     return (<div>
         <h1>Products</h1>
+        <select value={productData?.deptName} onChange={(e)=>dispatchProduct({type:"SET_DEPT",payload:e.target.value})}>
+            <option value="">Select</option>
+            <option value="Kitchen">Kitchen</option>
+            <option value="Clothing">Clothing</option> 
+            <option value="Toys">Toys</option>
+        </select>
+        <input onChange={(e)=>handleLowStock(e)} type="checkbox" />Low Stock items
+        <select value={productData?.deptName} onChange={(e)=>dispatchProduct({type:"SET_DEPT",payload:e.target.value})}>
+            <option value="">Sort Type</option>
+            <option value="name">Name</option>
+            <option value="price">Price</option> 
+            <option value="stock">Stock</option>
+        </select>
         <button onClick={()=>navigate("/newProduct")}>New</button>
         <table>
             <tr>
